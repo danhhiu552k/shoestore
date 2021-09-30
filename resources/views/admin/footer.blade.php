7f6264ff103b1026a3215a5b9dd381c0297335c5
XuanDanh <dangdanh55.dxd@gmail.com>
2021-09-30 11:43:08 +0700

    Thêm layout trang đăp nhập admin

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<script src="/template/admin/plugins/common/common.min.js"></script>
+<script src="/template/admin/js/custom.min.js"></script>
+<script src="/template/admin/js/settings.js"></script>
+<script src="/template/admin/js/gleek.js"></script>
+<script src="/template/admin/js/styleSwitcher.js"></script>
