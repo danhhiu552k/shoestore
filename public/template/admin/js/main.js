8c9ab92b347c2d318601a5471946a18f0a8d60de
XuanDanh <dangdanh55.dxd@gmail.com>
2021-10-11 10:51:00 +0700

    compelete add, edit, delete function

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -4,7 +4,6 @@ $.ajaxSetup({
     }
 });
 
-<<<<<<< HEAD
 /**
  * Upload Image
  */
@@ -50,32 +49,10 @@ function removeRow(id, url) {
                 if (result.error == false) {
                     alert(result.message);
                     location.reload();
-
                 } else {
-=======
-function removeRow(id,url)
-{
-    if(confirm('Bạn có muốn xóa thật không ?')){
-        $.ajax({
-            type:'DELETE',
-            datatype:'JSON',
-            data: { id },
-            url:url,
-            success:function(result){
-                if (result.error == false){
-                    alert(result.message);
-                    location.reload();
-
-                }
-                else{
->>>>>>> 5d00466800bcb12153d78cc97c7f97cc0ed94aa1
                     alert('Xóa không thành công');
                 }
             }
         })
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5d00466800bcb12153d78cc97c7f97cc0ed94aa1
--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -7,10 +7,5 @@
 <script src="/template/admin/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="/template/admin/dist/js/demo.js"></script>
-<<<<<<< HEAD
-=======
-<script src="/template/admin/main.js"></script>
-
->>>>>>> 5d00466800bcb12153d78cc97c7f97cc0ed94aa1
 <script src="/template/admin/js/main.js"></script>
 @yield('footer')
