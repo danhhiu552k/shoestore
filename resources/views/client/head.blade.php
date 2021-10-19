b4ba6e57f46c243c19b94a3d748477296518fad8
XuanDanh <dangdanh55.dxd@gmail.com>
2021-10-19 14:44:34 +0700

    Xây dựng chức năng đăng kí

--- resources/views/client/head.blade.php
+++ resources/views/client/head.blade.php
@@ -1,4 +1,3 @@
-
 <div class="header-top">
     <div class="container">
         <div class="header-left">
@@ -31,28 +30,75 @@
                         <li><a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a></li>
                         <li><a href="about">About Us</a></li>
                         <li><a href="/register"><i class="icon-user"></i>Đăng Kí</a></li>
-                        <li><a href="/login"><i class="icon-user"></i>Đăng Nhập</a></li>
+
                         @if (Session::get('login'))
-                            <li>
-                                <div>
-                                    <div class="dropdown">
-                                        <button class="dropdown-toggle" type="button"
-                                            id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                            {{ Session::get('firstname') }}
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <a class="dropdown-item" href="/logout">Đăng Xuất</a>
-                                        </div>
-                                    </div>
+                            <div class="dropdown">
+                                <button
+                                    class="nut_dropdown">{{ Session::get('firstname') . ' ' . Session::get('lastname') }}</button>
+                                <div class="noidung_dropdown">
+                                    <a href="/logout">Đăng Xuất</a>
                                 </div>
-                            </li>
+                            </div>
+                        @else
+                            <li><a href="/login"><i class="icon-user"></i>Đăng Nhập</a></li>
                         @endif
+
                     </ul>
                 </li>
             </ul><!-- End .top-menu -->
         </div><!-- End .header-right -->
     </div><!-- End .container -->
 </div><!-- End .header-top -->
+
+@section('footer')
+    <style>
+        /* Nút Dropdown*/
+        .nut_dropdown {
+            background-color: #ffffff;
+            color: white;
+            padding: 16px;
+            color: rgb(17, 16, 16);
+            font-size: 16px;
+            border: none;
+            color: rgb(0, 0, 0);
+            text-transform: uppercase;
+            font-size: 1.3rem;
+            font-weight: 300;
+        }
+
+        /* Thiết lập vị trí cho thẻ div với class dropdown*/
+        .dropdown {
+           
+        }
+
+        /* Nội dung Dropdown */
+        .noidung_dropdown {
+            /*Ẩn nội dung các đường dẫn*/
+            display: none;
+            position: absolute;
+            background-color: #f1f1f1;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        /* Thiết kế style cho các đường dẫn trong Dropdown */
+        .noidung_dropdown a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        /* thay đổi màu background khi hover vào đường dẫn */
+        .noidung_dropdown a:hover {
+            background-color: #ddd;
+        }
+
+        /* hiển thị nội dung dropdown khi hover */
+        .dropdown:hover .noidung_dropdown {
+            display: block;
+        }
+
+    </style>
+@endsection
