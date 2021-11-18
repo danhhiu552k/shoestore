969d5702cfed7ebbd7d15bd052e4842c77f7dff8
Danh Dang <dangdanh55.dxd@gmail.com>
2021-11-18 08:51:04 +0700

    Format code giỏ hàng

--- resources/views/client/menu.blade.php
+++ resources/views/client/menu.blade.php
@@ -9,7 +9,8 @@
                 </button>
 
                 <a href="\" class="logo">
-                    <img src="/template/client/images/logo/logo.png" class="social-icon mylogo" alt="Molla Logo" width="105" height="25">
+                    <img src="/template/client/images/logo/logo.png" class="social-icon mylogo" alt="Molla Logo"
+                         width="105" height="25">
                 </a>
             </div><!-- End .header-left -->
 
@@ -44,51 +45,61 @@
                     <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true"
                        aria-expanded="false" data-display="static">
                         <i class="icon-shopping-cart"></i>
-                        <span class="cart-count">{{!is_null(\Session::get('carts'))? count(\Session::get('carts')):0}}</span>
+                        <span
+                            class="cart-count">{{!is_null(\Session::get('carts'))? count(\Session::get('carts')):0}}</span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
-                        @php $sumPriceCart =0;
+                        @php
+                            $sumPriceCart =0;
                         @endphp
-                        <div class="dropdown-cart-products">
-                            @if(count($products) >0)
+                        @if(count($products))
+                            <div class="dropdown-cart-products">
                                 @foreach($products as $key => $product)
-                                    @php $price =\App\Helpers\Product::checkprice($product->price ,$product->price_sale,$product->quantity) ;
-                                    $sumPriceCart+=$product->price_sale!=0 ? $product->price_sale : $product->price;
+                                    @php
+                                        $sumPriceCart += $product->price_sale!=0 ? $product->price_sale : $product->price;
                                     @endphp
-                            <div class="product">
-                                <div class="product-cart-details">
-                                    <h4 class="product-title">
-                                        <a href="product.html">{{$product->name}}</a>
-                                    </h4>
+                                    <div class="product">
+                                        <div class="product-cart-details">
+                                            <h4 class="product-title">
+                                                <a href="product.html">{{$product->name}}</a>
+                                            </h4>
 
-                                    <span class="cart-product-info">
-                                        <span class="cart-product-qty">{!!$price!!}</span>
-
-                                    </span>
-                                </div><!-- End .product-cart-details -->
-
-                                <figure class="product-image-container">
-                                    <a href="product.html" class="product-image">
-                                        <img src="{{$product->thumb}}" alt="product">
-                                    </a>
-                                </figure>
-                                <a href="#" class="btn-remove" title="Remove Product"><i
-                                        class="icon-close"></i></a>
-                            </div><!-- End .product -->
+                                            <span class="cart-product-info">
+                                                <span class="cart-product-qty">
+                                                    {!!\App\Helpers\Product::checkprice($product->price ,$product->price_sale,$product->quantity)!!}
+                                                </span>
+                                            </span>
+                                        </div><!-- End .product-cart-details -->
+                                        <div>
+                                            <figure class="product-image-container">
+                                                <a href="product.html" class="product-image">
+                                                    <img src="{{$product->thumb}}" alt="product">
+                                                </a>
+                                            </figure>
+                                            <a href="#" class="btn-remove" title="Remove Product">
+                                                <i class="icon-close"></i>
+                                            </a>
+                                        </div><!-- End .product -->
+                                    </div>
                                 @endforeach
-                            @endif
-                        </div><!-- End .cart-product -->
-
+                            </div><!-- End .cart-product -->
+                        @else
+                            <div class="dropdown-cart-products">
+                                Không có sản phẩm
+                            </div>
+                        @endif
                         <div class="dropdown-cart-total">
-                            <span>Total</span>
+                            <span>Tổng</span>
 
-                            <span class="cart-total-price">{{number_format($sumPriceCart,'0','','.')}}</span>
+                            <span class="cart-total-price">
+                                {{number_format($sumPriceCart,'0','','.')}}
+                            </span>
                         </div><!-- End .dropdown-cart-total -->
 
                         <div class="dropdown-cart-action">
-                            <a href="/cart" class="btn btn-primary">View Cart</a>
-                            <a href="/check" class="btn btn-outline-primary-2"><span>Checkout</span><i
+                            <a href="/carts" class="btn btn-primary">Xem giỏ hàng</a>
+                            <a href="/check" class="btn btn-outline-primary-2"><span>Thanh toán</span><i
                                     class="icon-long-arrow-right"></i></a>
                         </div><!-- End .dropdown-cart-total -->
                     </div><!-- End .dropdown-menu -->
